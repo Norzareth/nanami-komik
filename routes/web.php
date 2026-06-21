b9f985a29642a73acc82a83a3b1523f919f32611
nawfu <veronviera4@gmail.com>
2026-06-21 21:30:04 +0700

    Route CRUD profil mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KomikController;
 use App\Http\Controllers\AuthController; // Dari yang kita buat di Step sebelumnya
 use App\Http\Controllers\PenggunaController;
+use App\Http\Controllers\ProfilController;
 
 // Route Utama (Komik)
 Route::get('/', [KomikController::class, 'index'])->name('home');
@@ -13,5 +14,9 @@ Route::get('/library', [KomikController::class, 'library'])->name('library');
 // Route Login
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'authenticate']);
+
+// Rute untuk melihat dan mengedit profil pribadi (untuk Mahasiswa/User)
+Route::get('/profil', [ProfilController::class, 'edit'])->name('profil.edit');
+Route::put('/profil/update', [ProfilController::class, 'update'])->name('profil.update');
 
 Route::resource('pengguna', PenggunaController::class);
