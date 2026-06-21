867aa56adff236c1d8ccc505df67f690c1b9516f
nawfu <veronviera4@gmail.com>
2026-06-21 21:10:51 +0700

    feat: setup database, model, dan logika CRUD backend pengguna

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -3,16 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Pengguna extends Authenticatable
 {
+    use Notifiable;
+
     protected $table = 'pengguna';
-    protected $primaryKey = 'id_user';
-    
+    protected $primaryKey = 'ID_User';
+
     protected $fillable = [
-        'nama_user',
-        'email_user',
+        'nama_user', 
+        'email_user', 
         'password',
-        'role_user',
+        'role_user'
     ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
 }
